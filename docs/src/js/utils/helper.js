cf134e5282482f8cfedf92cb837e0a5b5a88a0d9
Dongwoo Lee <likedemian@me.com>
2018-08-06 18:16:28 +0900

    Fix: interval of Methods

--- docs/src/js/utils/helper.js
+++ docs/src/js/utils/helper.js
@@ -1,12 +1,18 @@
 const $on = (target, type, callback) => {
   target.addEventListener(type, callback)
 }
+
+
 const $qs = (selector, scope = document) => {
   return scope.querySelector(selector);
 }
+
+
 const $qsa = (selector, scope = document) => {
   return scope.querySelectorAll(selector);
 }
+
+
 const $empty = (target) => {
   while (target.hasChildNodes()) {
     target.removeChild(target.firstChild);
