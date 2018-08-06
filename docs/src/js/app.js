92081a11ca35f2c37a9a24edc5cd3f7c3c9c3e6f
Dongwoo Lee <likedemian@me.com>
2018-08-06 18:13:56 +0900

    Add: contentsDetail Class

--- docs/src/js/app.js
+++ docs/src/js/app.js
@@ -1,26 +1,27 @@
 import { CategoriesView } from './view/CategoriesView.js';
-import { CategoriesController } from './controller/CategoriesController.js';
-
 import { MainSlidesView } from './view/MainSlidesView.js';
-import { MainSlidesController } from './controller/MainSlidesController.js';
-
 import { SlideListsView } from './view/SlideListsView.js';
+import { ContentsDetailView } from './view/ContentsDetailView.js'
+
+import { CategoriesController } from './controller/CategoriesController.js';
+import { MainSlidesController } from './controller/MainSlidesController.js';
 import { SlideListsController } from './controller/SlideListsController.js';
+import { ContentsDetailController } from './controller/ContentsDetailController.js';
 
 import { MovieData } from './model/DataURL.js';
 import { SlideListsModel } from './model/SlideListsModel.js';
 
 const movieData = new MovieData();
-
 const slideListsModel = new SlideListsModel(movieData)
 
+
 const categoriesView = new CategoriesView();
-const categoriesController = new CategoriesController(categoriesView);
-
-
 const mainSlidesView = new MainSlidesView();
-const mainSlidesController = new MainSlidesController(mainSlidesView, slideListsModel, movieData);
-
-
 const slideListsView = new SlideListsView();
+const contentsDetailView = new ContentsDetailView();
+
+
+const categoriesController = new CategoriesController(categoriesView);
+const mainSlidesController = new MainSlidesController(mainSlidesView, slideListsModel, movieData);
 const slideListsController = new SlideListsController(slideListsView, slideListsModel);
+const contentsDetailController = new ContentsDetailController(contentsDetailView)
