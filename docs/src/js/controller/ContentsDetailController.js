f0c8da13bd274051f825342c53e230f464412d52
Dongwoo Lee <likedemian@me.com>
2018-08-07 13:24:56 +0900

    Add: hover Features

--- docs/src/js/controller/ContentsDetailController.js
+++ docs/src/js/controller/ContentsDetailController.js
@@ -1,5 +1,28 @@
+import { $on, $qs, $qsa } from '../utils/helper.js';
+
 export class ContentsDetailController {
   constructor(contentsDetailView) {
     this.contentsDetailView = contentsDetailView;
+    this.cinemaList = $qs('.main__cinemas__list');
+
+    this.getInit();
   }
+
+
+  getInit() {
+    this.cinemaList.addEventListener('mouseover', this.expandDetail.bind(this));
+  }
+
+  expandDetail(event) {
+    if (event.target.className == 'contents__item__content__overlay') {
+      event.target.style.backgroundColor = '#888';
+      event.target.style.opacity = '0.3';
+
+      event.target.addEventListener('mouseleave', e => {
+        event.target.style.backgroundColor = '';
+        event.target.style.opacity = '1'
+      })
+    }
+  }
+
 }
--- docs/src/js/view/ContentsDetailView.js
+++ docs/src/js/view/ContentsDetailView.js
@@ -2,18 +2,10 @@ import { $on, $qs, $qsa } from '../utils/helper.js';
 
 export class ContentsDetailView {
   constructor() {
-    this.cinemaSlideLists = $qs('.main__cinemas__list');   
+    this.cinemaSlideLists = $qs('.main__cinemas__list');
   }
-
-  bindMouseOver(handler) {
-    this.cinemaSlideLists.addEventListener('mouseover', handler);    
+  bindExpandDetail(handler) {
+    this.cinemaSlideLists.addEventListener('mouseover', handler);
+    
   }
-
-  bindMouseLeave(handler) {
-    this.cinemaSlideLists.addEventListener('mouseleave', handler);    
-  }
-
-
-
-
 }
